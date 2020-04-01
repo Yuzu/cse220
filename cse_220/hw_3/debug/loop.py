e33c8c33c4ea985270c913d9c44957d1c6913c28
Tim Wu <t1mtimmy@live.com>
2020-04-01 13:54:03 -0400

    hw 3 part 2 finished

---
+++ cse_220/hw_3/debug/loop.py
@@ -0,0 +1,5 @@
+loops = 0
+for x in range(54):
+    loops += 1
+
+print(loops)
--- cse_220/hw_3/proj3.asm
+++ cse_220/hw_3/proj3.asm
@@ -209,6 +209,387 @@ load_game_file:
 
 # Part II
 save_game_file:
+	# $a0 has a pointer to a GameBoard
+	# $a1 has the output filename address.
+	
+	# Preserve $s args
+	addi $sp, $sp, -28
+	sw $s0, 0($sp) # Number of digits, used to keep track of how many times we push onto the stack.
+	sw $s1, 4($sp) # Used to preserve file descriptor.
+	sw $s2, 8($sp) # "i" in loop for rows
+	sw $s3, 12($sp) # stores row count
+	sw $s4, 16($sp) # used as pointer to gameboard.
+	sw $s5, 20($sp) # used to store column count
+	sw $s6, 24($sp) # "j" in loop for columns
+	
+	# Preserve args before syscall
+	addi $sp, $sp, -12 
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $ra, 8($sp)
+	
+	li $v0, 13
+	move $a0, $a1 # Move filename to a0
+	li $a1, 1 # Flag 1 for write
+	li $a2, 0 # Mode is ignored
+	syscall
+	
+	# Restore saved args
+	lw $a0, 0($sp)
+	lw $a1, 4($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	 
+	move $s1, $v0 # Store file descriptor in $s1
+	
+	bltz $s1, save_file_error # Neg return value indicates an error occurred.
+	j no_save_file_error
+	
+	save_file_error:
+		li $v0, -1
+		jr $ra
+		
+	no_save_file_error:
+	
+	# Loop through GameBoard. 
+	# Mod 10 to get 1s place -> push onto stack -> remainder is 0 -> pop everything off to get reverse order to store (make sure to convert to ascii)
+	
+	# Get size of board to determine where to loop to | board size is 1 + 1 + (rows) x (columns) x (2)
+	# Loop from 0 to (rows)(columns)(2) - 2
+	
+	lb $t1, 0($a0) # number of rows
+	# ***** SAVE THE ROW COUNT *****
+	li $t3, 10 # Used for mod division.
+	li $s0, 0 # Number of digits, used to keep track of how many times we push onto the stack.
+	
+	while_save_rows:
+	
+	beqz $t1, store_rows # We've dealt w/ all the digits, 0/10 = 0. now to store the digits in reverse order.
+	
+	div $t1, $t3 # rows / 10 | EXAMPLE: 254 / 10 
+	mfhi $t6 # Remainder, result of mod division | EXAMPLE: 4 stored
+	mflo $t1 # Integer divison result | EXAMPLE: 25 stored, divide this by 10 next loop.
+	
+	# New digit, need to push onto stack.
+	
+	# Convert digit to ascii first.
+	addi $t6, $t6, 48 # Convert to ascii
+	addi $sp, $sp, -4
+	sw $t6, 0($sp)
+	addi $s0, $s0, 1 # Increment number of pushes.
+	j while_save_rows
+	
+	store_rows:
+	# Also clear stack, we need to pop the stuff we pushed.
+	beqz $s0, while_save_rows_done
+	move $fp, $sp # Store current stack pointer in $fp, we're gonna be pushing stuff onto the stack.
+		
+	# Preserve args before syscall
+	addi $sp, $sp, -12 
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $ra, 8($sp)
+		
+	li $v0, 15
+	move $a0, $s1 # file descriptor
+	move $a1, $fp # address of buffer
+	li $a2, 1 # max characters = hardcoded 1
+	syscall
+		
+	# Restore saved args
+	lw $a0, 0($sp)
+	lw $a1, 4($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	
+	addi $sp, $sp, 4 # Pop a character off the stack
+	addi $s0, $s0, -1
+	j store_rows
+	
+	store_rows_done:
+	
+	
+	while_save_rows_done:
+	move $fp, $sp # Restore $fp
+	
+	# ***** SAVE A SPACE IN THE FILE ******
+	# Preserve args before syscall
+	addi $sp, $sp, -12 
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $ra, 8($sp)
+	
+	li $t0, 32 # " " in ascii
+	addi $sp, $sp, -4 # push space onto stack
+	sw $t0, 0($sp)
+	
+	li $v0, 15
+	move $a0, $s1 # file descriptor
+	move $a1, $sp # file buffer
+	li $a2, 1 # hardcoded len of 1
+	syscall 
+	
+	addi $sp, $sp, 4 # get rid of buffer
+	
+	# Restore saved args
+	lw $a0, 0($sp)
+	lw $a1, 4($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	
+	# *****SAVE COLUMN COUNT IN FILE******
+	
+	lb $t1, 1($a0) # number of columns
+	
+	li $t3, 10 # Used for mod division.
+	li $s0, 0 # Number of digits, used to keep track of how many times we push onto the stack.
+	
+	while_save_columns:
+	
+	beqz $t1, store_columns # We've dealt w/ all the digits, 0/10 = 0. now to store the digits in reverse order.
+	
+	div $t1, $t3 # rows / 10 | EXAMPLE: 254 / 10 
+	mfhi $t6 # Remainder, result of mod division | EXAMPLE: 4 stored
+	mflo $t1 # Integer divison result | EXAMPLE: 25 stored, divide this by 10 next loop.
+	
+	# New digit, need to push onto stack.
+	
+	# Convert digit to ascii first.
+	addi $t6, $t6, 48 # Convert to ascii
+	addi $sp, $sp, -4
+	sw $t6, 0($sp)
+	addi $s0, $s0, 1 # Increment number of pushes.
+	j while_save_columns
+	
+	store_columns:
+	# Also clear stack, we need to pop the stuff we pushed.
+	beqz $s0, while_save_columns_done
+	move $fp, $sp # Store current stack pointer in $fp, we're gonna be pushing stuff onto the stack.
+		
+	# Preserve args before syscall
+	addi $sp, $sp, -12 
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $ra, 8($sp)
+		
+	li $v0, 15
+	move $a0, $s1 # file descriptor
+	move $a1, $fp # address of buffer
+	li $a2, 1 # max characters = hardcoded 1
+	syscall
+		
+	# Restore saved args
+	lw $a0, 0($sp)
+	lw $a1, 4($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	
+	addi $sp, $sp, 4 # Pop a character off the stack
+	addi $s0, $s0, -1
+	j store_columns
+	
+	store_columns_done:
+	
+	while_save_columns_done:
+	move $fp, $sp # Restore $fp
+	
+	# ***** SAVE A NEWLINE IN THE FILE ******
+	# Preserve args before syscall
+	addi $sp, $sp, -12 
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $ra, 8($sp)
+	
+	li $t0, 10 # newline char in ascii
+	addi $sp, $sp, -4 # push space onto stack
+	sw $t0, 0($sp)
+	
+	li $v0, 15
+	move $a0, $s1 # file descriptor
+	move $a1, $sp # file buffer
+	li $a2, 1 # hardcoded len of 1
+	syscall 
+	
+	addi $sp, $sp, 4 # get rid of buffer
+	
+	# Restore saved args
+	lw $a0, 0($sp)
+	lw $a1, 4($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	
+	
+	# ***** STORE TILE VALUES *****
+	
+	lb $s3, 0($a0)
+	lb $s5, 1($a0)
+	
+	# file descriptor still in $s1
+	li $s2, 0 # "i" to count rows
+	#rows in $s3
+	move $s4, $a0 # use $s4 as pointer to gameboard
+	addi $s4, $s4, 2 # Look past first two bytes. We already looked at those.
+	# columns in $s5
+	# "i" to count columns in $s6
+	
+	# Prevent OBOE
+	addi $s5, $s5, -1 
+	
+	for_save_file_tiles:
+		
+		beq $s2, $s3, for_save_file_tiles_done # finished all rows
+		
+		li $t3, 10 # Used for mod division.
+		li $s0, 0 # Number of digits, used to keep track of how many times we push onto the stack.
+		lh $t1, 0($s4) # Load current short.
+		
+		beqz $t1, store_zero # Current short is 0, we need to store a 0 instead of going through all this.
+		j while_save_tiles
+		
+		store_zero:
+		# ***** SAVE A ZERO IN THE FILE ******
+		# Preserve args before syscall
+		addi $sp, $sp, -12 
+		sw $a0, 0($sp)
+		sw $a1, 4($sp)
+		sw $ra, 8($sp)
+	
+		li $t0, 48 # "0" in ascii
+		addi $sp, $sp, -4 # push space onto stack
+		sw $t0, 0($sp)
+	
+		li $v0, 15
+		move $a0, $s1 # file descriptor
+		move $a1, $sp # file buffer
+		li $a2, 1 # hardcoded len of 1
+		syscall 
+	
+		addi $sp, $sp, 4 # get rid of buffer
+	
+		# Restore saved args
+		lw $a0, 0($sp)
+		lw $a1, 4($sp)
+		lw $ra, 8($sp)
+		addi $sp, $sp, 12
+		
+		j while_save_tiles_done
+		
+		while_save_tiles:
+			
+			beqz $t1, store_tiles # We've dealt w/ all the digits, 0/10 = 0. now to store the digits in reverse order.
+	
+			div $t1, $t3 # rows / 10 | EXAMPLE: 254 / 10 
+			mfhi $t6 # Remainder, result of mod division | EXAMPLE: 4 stored
+			mflo $t1 # Integer divison result | EXAMPLE: 25 stored, divide this by 10 next loop.
+	
+			# New digit, need to push onto stack.
+	
+			# Convert digit to ascii first.
+			addi $t6, $t6, 48 # Convert to ascii
+			addi $sp, $sp, -4
+			sw $t6, 0($sp)
+			addi $s0, $s0, 1 # Increment number of pushes.
+			j while_save_tiles
+	
+			store_tiles:
+			# Also clear stack, we need to pop the stuff we pushed.
+			beqz $s0, while_save_tiles_done
+			move $fp, $sp # Store current stack pointer in $fp, we're gonna be pushing stuff onto the stack.
+		
+			# Preserve args before syscall
+			addi $sp, $sp, -12 
+			sw $a0, 0($sp)
+			sw $a1, 4($sp)
+			sw $ra, 8($sp)
+		
+			li $v0, 15
+			move $a0, $s1 # file descriptor
+			move $a1, $fp # address of buffer
+			li $a2, 1 # max characters = hardcoded 1
+			syscall
+		
+			# Restore saved args
+			lw $a0, 0($sp)
+			lw $a1, 4($sp)
+			lw $ra, 8($sp)
+			addi $sp, $sp, 12
+	
+			addi $sp, $sp, 4 # Pop a character off the stack
+			addi $s0, $s0, -1
+			j store_tiles
+	
+			store_tiles_done:
+	
+		while_save_tiles_done:
+		
+	# Look at the next tile.
+	addi $s4, $s4, 2 # Look at next tile
+	
+	beq $s6, $s5, end_of_row # if this is the end of the row, write a newline char, reset column counter ($s6), and increment row counter ($s2)
+	addi $s6, $s6, 1 # Increment column counter
+	j not_end_of_row
+	
+	end_of_row:
+	
+	# ***** SAVE A NEWLINE IN THE FILE ******
+	# Preserve args before syscall
+	addi $sp, $sp, -12 
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $ra, 8($sp)
+	
+	li $t0, 10 # newline char in ascii
+	addi $sp, $sp, -4 # push space onto stack
+	sw $t0, 0($sp)
+	
+	li $v0, 15
+	move $a0, $s1 # file descriptor
+	move $a1, $sp # file buffer
+	li $a2, 1 # hardcoded len of 1
+	syscall 
+	
+	addi $sp, $sp, 4 # get rid of buffer
+	
+	# Restore saved args
+	lw $a0, 0($sp)
+	lw $a1, 4($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	
+	li $s6, 0 # reset column counter
+	addi $s2, $s2, 1 # increment row counter
+	
+	j for_save_file_tiles
+	
+	not_end_of_row:
+	# ***** SAVE A SPACE IN THE FILE ******
+	# Preserve args before syscall
+	addi $sp, $sp, -12 
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $ra, 8($sp)
+	
+	li $t0, 32 # " " in ascii
+	addi $sp, $sp, -4 # push space onto stack
+	sw $t0, 0($sp)
+	
+	li $v0, 15
+	move $a0, $s1 # file descriptor
+	move $a1, $sp # file buffer
+	li $a2, 1 # hardcoded len of 1
+	syscall 
+	
+	addi $sp, $sp, 4 # get rid of buffer
+	
+	# Restore saved args
+	lw $a0, 0($sp)
+	lw $a1, 4($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	
+	j for_save_file_tiles
+	
+	for_save_file_tiles_done:
 jr $ra
 
 # Part III
