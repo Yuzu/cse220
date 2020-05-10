3345d9629fe1f4125f474c4d24d20ca85e77afd1
Tim Wu <t1mtimmy@live.com>
2020-05-10 13:50:04 -0400

    hw 5 final commit

--- cse_220/hw_5/append_main.asm
+++ cse_220/hw_5/append_main.asm
@@ -1,23 +1,9 @@
 .data
-num: .word 37
+num: .word 94
 list:
-.word 5  # list's size
-.word node761 # address of list's head
-node168:
-.word 402
-.word 0
-node814:
-.word 978
-.word node248
-node761:
-.word 962
-.word node112
-node248:
-.word 526
-.word node168
-node112:
-.word 762
-.word node814
+.word 0  # list's size
+.word 0  # address of list's head (null)
+
 
 
 newline: .asciiz "\n"
--- cse_220/hw_5/insert_main.asm
+++ cse_220/hw_5/insert_main.asm
@@ -1,27 +1,27 @@
 .data
-num: .word 43
-index: .word 6
+num: .word 24
+index: .word 3
 list:
 .word 6  # list's size
-.word node619 # address of list's head
-node140:
-.word 863
-.word node940
-node940:
-.word 140
-.word node21
-node209:
-.word 332
-.word node147
-node619:
-.word 59
-.word node209
-node147:
-.word 655
-.word node140
-node21:
-.word 846
+.word node761 # address of list's head
+node130:
+.word 293
 .word 0
+node248:
+.word 526
+.word node168
+node112:
+.word 762
+.word node814
+node814:
+.word 978
+.word node248
+node761:
+.word 962
+.word node112
+node168:
+.word 402
+.word node130
 
 
 newline: .asciiz "\n"
